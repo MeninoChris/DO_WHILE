52cfa2f427de1762c93cebd19d63b22e62f2cb57
MeninoChris <159173970+MeninoChris@users.noreply.github.com>
2025-03-19 16:35:46 -0300

    Update EX1_contador.java

--- EX1_contador.java
+++ EX1_contador.java
@@ -1,6 +1,6 @@
 public class EX1_contador {
     public static void main(String[] args){
-        int contador = 0;
+        int contador = 100;
 
         while (contador <= 200){
             System.out.println(contador);
